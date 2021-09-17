00204bd8a88b4999a95e96b6188c64e39a265797
Aaris.Baskaran <aaris.baskaran@gmail.com>
2021-09-17 16:52:56 +1200

    added Position class

--- src/domain/Position.java
+++ src/domain/Position.java
@@ -1,2 +1,35 @@
-package domain;public class Position {
+package domain;
+
+/**
+ * A position on the maze.
+ *
+ * @author aarisbaskaran
+ */
+
+public class Position {
+    private final int x;
+    private final int y;
+
+    /**
+     * Construct position on the maze
+     *
+     * @param x
+     * @param y
+     */
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
 }
