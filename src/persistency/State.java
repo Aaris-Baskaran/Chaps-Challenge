63b8f476f7f0ebbda3a4867ea895213942d9a409
KVRiver <kvuong7work@gmail.com>
2021-09-21 11:57:09 +1200

    Created State and StateManager which can read level1.xml

--- src/persistency/State.java
+++ src/persistency/State.java
@@ -1,5 +1,77 @@
 package persistency;
 
-public class State {
+import domain.Position;
 
+public class State {
+	
+	/**
+    * the current level in State
+    */
+	private final int levelNum;
+	/**
+    * the time left in Game
+    */
+	private final int maxTime;
+	/**
+    * the maze layout in String
+    */
+	private final String[] maze;
+	/**
+    * the chapPos field in Game
+    */
+	private final Position chapPos;
+	/**
+    * the chapDirection field in Game
+    */
+	private final char chapDirection;
+	/**
+    * the chips remaining in Game
+    */
+	private final int chipsRemaining;
+	
+	/**
+     * @param levelNum current level
+     * @param maxTime time left
+     * @param maze the maze layout
+     * @param pos position of Chap
+     * @param dir direction of Chap
+     * @param keys keys that Chap holds
+     * @param chipsRemaining chips left on maze
+     */
+	public State(int levelNum, int maxTime, String[] maze, int x, int y, char dir, int chipsRemaining){
+	   this.levelNum = levelNum;
+	   this.maxTime = maxTime;
+	   this.maze = maze;
+	   this.chapPos = new Position(x, y);
+	   this.chapDirection = dir;
+	   this.chipsRemaining = chipsRemaining;
+	}
+	
+	/**
+     * Gets the level number
+     * @return the level number
+     */
+    public int getLevelNum() {
+        return levelNum;
+    }
+    
+    public int getTime() {
+    	return maxTime;
+    }
+    
+    public String[] getMaze() {
+    	return maze;
+    }
+    
+    public Position getPos() {
+    	return chapPos;
+    }
+    
+    public char getDir() {
+    	return chapDirection;
+    }
+    
+    public int getChipsLeft() {
+    	return chipsRemaining;
+    }
 }
---
+++ src/persistency/StateManager.java
@@ -0,0 +1,72 @@
+package persistency;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
+public class StateManager {
+	
+	private static File[] files;
+	
+	public StateManager() {
+		files = findFiles();
+	}
+	
+	private File[] findFiles() {
+		File folder = new File("levels");
+		return folder.listFiles();
+	}
+	
+	public State loadState() {
+		
+		State state = null;
+		
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		
+		try {
+			
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			
+			Document doc = db.parse(files[0]);
+			
+			System.out.println(doc.getDocumentElement().getNodeName());
+			
+			Element element = doc.getDocumentElement();
+			
+			String level = element.getElementsByTagName("Level").item(0).getTextContent();
+			String time = element.getElementsByTagName("Time").item(0).getTextContent();
+			
+			String[] maze = new String[10];
+			for(int row = 0; row < 10; ++row) {
+				maze[row] = element.getElementsByTagName("Row").item(row).getTextContent();
+			}
+			
+			String x = element.getElementsByTagName("X").item(0).getTextContent();
+			String y = element.getElementsByTagName("Y").item(0).getTextContent();
+			String dir = element.getElementsByTagName("Dir").item(0).getTextContent();
+			String chips = element.getElementsByTagName("Chips").item(0).getTextContent();
+			
+			char direction = dir.charAt(0);
+			
+			int levelNum = Integer.parseInt(level);
+			int timeLeft = Integer.parseInt(time);
+			int xPos = Integer.parseInt(x);
+			int yPos = Integer.parseInt(y);
+			int chipsLeft = Integer.parseInt(chips);
+			
+			state = new State(levelNum, timeLeft, maze, xPos, yPos, direction, chipsLeft);
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			e.printStackTrace();
+		}
+		
+		return state;		
+	}
+}
